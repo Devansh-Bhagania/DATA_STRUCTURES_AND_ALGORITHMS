fa6cba4fa18ba713fa4578c587e153f452ca54d7
Devansh Bhagania <117916160+Devansh-Bhagania@users.noreply.github.com>
2023-08-09 23:00:56 +0530

    variable intro

--- intro_to_cpp/new1.cpp
+++ intro_to_cpp/new1.cpp
@@ -4,10 +4,8 @@ using namespace std;
 int main() {
   
   int a=45;
-  cout << "hello world !";
-  cout << "Hello DSA WORLD";
-  int a=5;
-  // why new lin eis not good 
-  // project file 
+  int myNum;
+myNum = 15;
+cout << myNum;
   return 0;
 }
