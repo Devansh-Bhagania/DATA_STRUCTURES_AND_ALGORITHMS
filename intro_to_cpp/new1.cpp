516976e2217f2eb0daeee328bfcee6f392f2b8ec
Devansh Bhagania <117916160+Devansh-Bhagania@users.noreply.github.com>
2023-08-14 22:21:48 +0530

    typescript and next js

--- intro_to_cpp/new1.cpp
+++ intro_to_cpp/new1.cpp
@@ -14,5 +14,6 @@ int j = 78;
 // tomorroe we will start dsa all the best 
 // netflix clone will also be started tomorrow 8 pm 
 // might have to make a discord channel to pratice dsa online 
+// today jannat travels is completed and revised next js also started typescript videos 
 return 0;
 }
