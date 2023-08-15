ab5a7521a361183c1bf98a44be66c206462465b8
Devansh Bhagania <117916160+Devansh-Bhagania@users.noreply.github.com>
2023-08-15 22:35:41 +0530

    yes yes

--- intro_to_cpp/new1.cpp
+++ intro_to_cpp/new1.cpp
@@ -15,5 +15,6 @@ int j = 78;
 // netflix clone will also be started tomorrow 8 pm 
 // might have to make a discord channel to pratice dsa online 
 // today jannat travels is completed and revised next js also started typescript videos 
+// ohhh jkn 
 return 0;
 }
