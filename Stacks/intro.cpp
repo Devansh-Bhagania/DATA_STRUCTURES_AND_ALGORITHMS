fc911b2cccb3e262b35ddc7abe6ddada8506c08f
Devansh Bhagania <117916160+Devansh-Bhagania@users.noreply.github.com>
2023-09-08 13:20:03 +0530

    stack started

---
+++ Stacks/intro.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#define n 100
+
+class stack{
+
+    int* arr;
+    int top;
+
+    public:
+    stack(){
+        arr= new int[n];
+        top=-1;
+    }
+};
