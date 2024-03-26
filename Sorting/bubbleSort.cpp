8678fc3bab0d96c8da37ace54ee554a21d4d625f
Devansh Bhagania <devanshbhagania19@gmail.com>
2024-03-26 17:43:48 +0530

    bubble sort theory

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n){
+        for(int i = 0; i<=n-1;i++){
+            int j = i;
+            while(j>0 && arr[j-1] > arr[j]){
+                int temp = arr[j-1];
+                arr[j-1]= arr[j];
+                arr[j] = temp;
+                j--; 
+            }
+        }
+}
+
+
+int main(){
+    cout <<"hello enter number of elements \n";
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i =0; i<n; i++){
+        cin >> arr[i];
+    }
+    // n = 5;
+    // int arr[5] = {1,4,3,6,5};
+    insertionSort(arr, n);
+    for (int i =0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+
+// time complexity is 0(n^2) 
